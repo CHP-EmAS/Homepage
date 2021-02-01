877cbc9efc079caf16f45628283fa473da110bdd
Clemens PC <clemens@huebner-clan.de>
2021-02-01 16:44:10 +0100

    add files to route manager

--- src_typescript/routes/routes.ts
+++ src_typescript/routes/routes.ts
@@ -7,6 +7,7 @@ routes.use('/css', express.static('static/css'));
 routes.use('/fonts', express.static('static/fonts'));
 routes.use('/js', express.static('static/js'));
 routes.use('/img', express.static('static/img'));
+routes.use('/files', express.static('static/files'));
 
 routes.get("/", function(req, res) {
     res.sendFile(path.join(__dirname + '/../../static/html/homepage.html'));

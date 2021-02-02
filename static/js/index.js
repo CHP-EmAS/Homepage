2ec8267b372ceff0c5e07bbb5c82eb36349c7ff2
Clemens PC <clemens@huebner-clan.de>
2021-02-02 12:51:37 +0100

    calculate right height offset

--- static/js/index.js
+++ static/js/index.js
@@ -126,7 +126,7 @@ $(function() {
 
   // SCROLL ANIMATIONS
   function onScrollInit(items, elemTrigger) {
-    var offset = $(window).height() / 1.6;
+    var offset = $(window).height() / 1.2;
     items.each(function() {
       var elem = $(this),
         animationClass = elem.attr('data-animation'),
